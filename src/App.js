2f4e4f9092265261091c932f40144fdac767e42b
Jacob McKenna <jacob99@live.ca>
2025-01-13 15:32:19 -0500

    added rough main

--- src/App.js
+++ src/App.js
@@ -1,37 +1,70 @@
-// import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import profilePhoto from './profile.jpg'; // Ensure the profile image is in the public or src folder
 
 function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-      <p>This is Jacob's first deployed React site.</p>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sarah" element={<SarahPage />} />
+    </Routes>
+  );
+}
+
+function Home() {
+  return (
+    <div className="app-container">
+      <header className="header">
+        <Link to="/sarah">
+          <button className="special-button">Special</button>
+        </Link>
+      </header>
+      <div className="profile-container">
+        <img src={profilePhoto} alt="Profile" className="profile-photo" />
+        <h1 className="name">Jacob Mckenna</h1>
+        <p className="description">
+          Yup, that's me. Welcome to my home sweet home.
+          <br />
+          Enjoy your stay here. Have a complimentary hot chocolate down below for visiting.
+          <br />
+          Good luck picking it up though!
+        </p>
+        <img
+          src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmk1MmsyZWl5OThiajV2Nzl1aHQ3dzJ3Y3VmOW04anJ1bm9tNXAyZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/L2NVl4obS7OgKUcuza/giphy.gif"
+          alt="Hot Chocolate GIF"
+          className="hot-chocolate-gif"
+        />
+        <p className="source-link">
+          <a
+            href="https://giphy.com/stickers/cup-hot-chocolate-zandraart-L2NVl4obS7OgKUcuza"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
+
+function SarahPage() {
+  return (
+    <div className="app-container">
+      <header className="header">
+        <Link to="/">
+          <button className="special-button">Back</button>
+        </Link>
+      </header>
+      <div className="profile-container">
+        <h1 className="name">Welcome to Sarah's Page!</h1>
+        <p className="description">
+          This page is under construction, but stay tuned for more!
+        </p>
+      </div>
+    </div>
+  );
+}
+
 export default App;
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
